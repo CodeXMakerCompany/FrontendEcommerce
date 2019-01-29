b65bc25ef7078d2831aae730578daf0caa1ebb0c
CodeXMaker <35282387+Stardie@users.noreply.github.com>
2019-01-28 21:31:47 -0600

    subiendo proyecto

---
+++ vistas/js/chatBox.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#hideshow').on('click', function(event) {        
+     $('.content').toggle('show');
+  });
+});
+$(document).ready(function(){
+  $('#hideshow2').on('click', function(event) {        
+     $('.content2').toggle('show');
+  });
+});
---
+++ vistas/modulos/chatBox.php
@@ -0,0 +1,21 @@
+
+ <?php 
+
+$servidor =  Ruta::ctrRutaServidor();
+$url =  Ruta::ctrRuta();
+
+ ?>
+ <section class="chat-container">
+       
+ <button class="chat-button" id='hideshow' data-toggle="tooltip" title="Como podemos ayudarte?"><img src="<?php echo $url; ?>vistas/img/plantilla/icons8-facebook-messenger-50.png"></button>
+  <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCodexmaker-1357852354360824%2F&tabs=messages&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border: 2px solid black; border-radius: 5px; display: none;" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" class="content" style="border:0;display:none"></iframe>
+  <br/>
+ 
+</div>
+  
+   </section>
+   
+
+  
+
+
